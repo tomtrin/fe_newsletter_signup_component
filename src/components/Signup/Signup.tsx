323448e99b21d06500a7b4c1d1d7cf8196d6bc0e
Tommy Trinh <tommy.trinh@pm.me>
2023-06-19 22:26:32 -0400

    feature: add tailwind css

---
+++ src/components/Signup/Signup.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-red-200'>
+      aaa
+    </div>
+  );
+}
+
+export default Signup;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {
             fontFamily: {
